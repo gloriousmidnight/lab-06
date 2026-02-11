cc1645189655f6f6add113f103a8c1bd83406b6b
Cassie Burke <ceburke@ualberta.ca>
2026-02-10 18:39:55 -0700

    finished lab demo content

---
+++ app/src/main/java/com/example/listycity/City.java
@@ -0,0 +1,28 @@
+package com.example.listycity;
+
+/**
+ * This is a class that defines a City
+ */
+public class City implements Comparable<City> {
+    private String city;
+    private String province;
+
+    City(String city, String province) {
+        this.city = city;
+        this.province = province;
+    }
+
+    public String getCityName() {
+        return this.city;
+    }
+
+    public String getProvinceName() {
+        return this.province;
+    }
+
+    @Override
+    public int compareTo(City o) {
+        City city = (City) o;
+        return this.city.compareTo(city.getCityName());
+    }
+}
